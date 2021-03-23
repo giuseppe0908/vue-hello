702fb1ef58c00406610ee53e8d3276c9980cb3c1
giuseppe <placidagiuseppe@gmail.com>
2021-03-23 16:22:04 +0100

    bonus completato

---
+++ main.js
@@ -0,0 +1,12 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+var app = new Vue(
+  {
+    el:'#root',
+    data: {
+      message:'Benvenuti io sono Vue 👨‍💻',
+      srcc:"papera.png",
+      descrizione:'immagine papera assassina 🐤',
+
+    }
+  }
+)
